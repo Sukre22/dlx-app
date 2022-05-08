dbe7aec6005d517ba3637a07e4051afcedac2a14
Sukre22 <trackandfieldd@gmail.com>
2022-05-08 17:42:07 +0300

    finished top-menu

--- src/components/app-menu-top/app-menu-top.js
+++ src/components/app-menu-top/app-menu-top.js
@@ -7,30 +7,92 @@ import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import EmojiObjectsIcon from '@mui/icons-material/EmojiObjects';
+import Chip from '@mui/material/Chip';
+import Stack from '@mui/material/Stack';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import AccountCircle from '@mui/icons-material/AccountCircle';
 
-export default class AppMenuTop extends Component {
 
-    render() {
+export default function AppMenuTop() {
+
+
+
+
+        const handleClick = () => {
+            console.info('You clicked the Chip.');
+        };
+
+    const [auth, setAuth] = React.useState(true);
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleChange = (event) => {
+        setAuth(event.target.checked);
+    };
+
+    const handleMenu = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+
+
 
         return(
           <div class="menu-top">
               <div class="menu-top_icons">
+
                   <div class="menu-top_icons3">
                       <MenuIcon />
                   </div>
                   <div class="menu-top_icons2">
                       <EmojiObjectsIcon fontSize="large"/>
-                      <p >Курс по Dialux Evo/Dialux 4.13</p>
+                      <p >Обучающий курс по Dialux Evo/Dialux 4.13</p>
                   </div>
 
+              </div>
+              <div class="menu-top_icons1">
+                  <Stack direction="row" spacing={1}>
+                      <Chip label="Dialux Evo" onClick={handleClick} />
+                      <Chip label="Dialux 4.13" variant="outlined" onClick={handleClick} />
+                  </Stack>
+                  <IconButton
+                      size="large"
+                      aria-label="account of current user"
+                      aria-controls="menu-appbar"
+                      aria-haspopup="true"
+                      onClick={handleMenu}
+                      color="inherit"
+                  >
+                      <AccountCircle />
+                  </IconButton>
+                  <Menu
+                      id="menu-appbar"
+                      anchorEl={anchorEl}
+                      anchorOrigin={{
+                          vertical: 'top',
+                          horizontal: 'right',
+                      }}
+                      keepMounted
+                      transformOrigin={{
+                          vertical: 'top',
+                          horizontal: 'right',
+                      }}
+                      open={Boolean(anchorEl)}
+                      onClose={handleClose}
+                  >
 
-
+                      <MenuItem onClick={handleClose}>Выход</MenuItem>
+                  </Menu>
               </div>
 
 
           </div>
         )
-    }
+
 
 
 }
--- src/components/app-menu/app-menu.js
+++ src/components/app-menu/app-menu.js
@@ -19,26 +19,26 @@ export default class AppMenu extends Component {
 					
 			<ul class="list-unstyled">
 
-			   <li class="week" id="week2">1 Введение</li>
-			   <li class="week">2 Основные понятия и определения</li>
-			   <li class="week">3 Знакомство с интерфейсом программы</li>
+			   <li class="week" id="week2" id="week3">1 Введение</li>
+			   <li class="week" id="week3" >2 Основные понятия и определения</li>
+			   <li class="week" id="week3">3 Знакомство с интерфейсом программы</li>
 			   <li class="week">4 Моделирование освещаемого объекта</li>
 			   <li class="second">4.1 Импорт чертежей различных форматов</li>
 			   <li class="second">4.2 Построение зданий и его помещений</li>
 			   <li class="second">4.3 Добавление дверей, окон, потолков и крыши</li>
 			   <li class="second">4.4 Добавление 3D объектов</li>
-			   <li class="second">4.5 Применение материалов и цветов</li>
+			   <li class="second" id="week3">4.5 Применение материалов и цветов</li>
 			   <li class="week">5 Расчет освещения</li>
 			   <li class="second">5.1 Расстановка и настройка светильников</li>
 			   <li class="second">5.2 Создание расчетных поверхностей</li>
 			   <li class="second">5.3 Настройка результатов расчета</li>
-			   <li class="second">5.4 Расчет аварийного освещения</li>
-			   <li class="week">6 Создание отчета</li>
+			   <li class="second" id="week3">5.4 Расчет аварийного освещения</li>
+			   <li class="week" id="week3">6 Создание отчета</li>
 			   <li class="week">7 Практика</li>
 			   <li class="second">7.1 Расчет наружного освещения</li>
 			   <li class="second">7.2 Расчет периметрального освещения</li>
 			   <li class="second">7.3 Расчет спортивной площадки с учетом UGR-показателя</li>
-			   <li class="second">7.4 Расчет спортивной площадки с учетом GR-показателя</li>
+			   <li class="second" id="week3">7.4 Расчет спортивной площадки с учетом GR-показателя</li>
 			   <li class="week" id="week5">8 Заключение</li>
 			</ul>
 	 </div>
