e50c5f3b428c2e4049f977d52c96f114132d18ef
Sukre22 <trackandfieldd@gmail.com>
2022-05-15 15:43:14 +0300

    add page 1

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,6 +3,7 @@ import './app.css';
 
 import AppMenu from '../app-menu/app-menu.js';
 import AppMenuTop from '../app-menu-top/app-menu-top.js';
+import AppMenuPages2 from '../app-menu-pages/app-menu-pages-2/app-menu-pages-2.js';
 import AppMenuPages1 from '../app-menu-pages/app-menu-pages-1/app-menu-pages-1.js';
 
 export default class App extends Component {
@@ -13,7 +14,9 @@ export default class App extends Component {
            <>
                <AppMenuTop/>
                <AppMenu/>
+               {/* <AppMenuPages2/>*/}
                <AppMenuPages1/>
+
            </>
 
        )
