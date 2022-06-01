fcb1e9dde92964f941e5211fe0094761d2542f98
Sukre22 <trackandfieldd@gmail.com>
2022-06-01 22:50:16 +0300

    maded on hooks

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -8,57 +8,7 @@ import AppMenuPages2 from '../app-menu-pages/app-menu-pages-2/app-menu-pages-2.j
 import AppMenuPages1 from '../app-menu-pages/app-menu-pages-1/app-menu-pages-1.js';
 import AppHome from '../app-home/app-home.js';
 
-{/*export default class App extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {            
-            burger: false           
-        };
-        
-        this.onBurger = this.onBurger.bind(this);        
-    }
-
-    onBurger(e) {
-        this.setState(({burger}) => ({
-            burger: !burger
-           
-        }))
-        console.log(this.state.burger);
-    }
-
-   render() {
-
-       return(
-           <>
-               <Router>
-                   <AppMenuTop
-                   onMenuOff={this.state.burger}
-                   onBurgerChange={this.onBurger}  
-                   />
-                   <AppMenu
-                   onMenuOff={this.state.burger}                 
-                   />
-                   <Routes>
-
-
-                       <Route path='/dlxevo/introduction' element={<AppMenuPages1
-                           onMenuOff={this.state.burger}
-                       />}/>
-                       <Route path="/dlxevo/guide" element={<AppMenuPages2
-                           onMenuOff={this.state.burger}
-                       />}/>
-                   </Routes>
-
-               </Router>
-
-           </>
-
-       )
-   }
-
-
-}*/}
 
 const App = (props) => {
 
