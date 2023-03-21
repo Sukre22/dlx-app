3f2065a842fbff7346412d4c026a118365bcc0b8
Исаев Раиль Рамилевич <r.isaev@ledel.local>
2023-03-21 07:57:22 +0300

    upgrate menu

--- src/components/app-menu/app-menu.js
+++ src/components/app-menu/app-menu.js
@@ -181,15 +181,10 @@ const AppMenu = ({onBurgerChange, onMenuOff}) => {
 			   <li class="app__menu-list-part end">4.5 Применение материалов и цветов</li>
 			   <li class="app__menu-list-head">5 Расчет освещения</li>
 			   <li class="app__menu-list-part">5.1 Расстановка и настройка светильников</li>
-			   <li class="app__menu-list-part">5.2 Создание расчетных поверхностей</li>
-			   <li class="app__menu-list-part">5.3 Настройка результатов расчета</li>
-			   <li class="app__menu-list-part end">5.4 Расчет аварийного освещения</li>
-			   <li class="app__menu-list-head end">6 Создание отчета</li>
-			   <li class="app__menu-list-head">7 Практика</li>
-			   <li class="app__menu-list-part">7.1 Расчет наружного освещения</li>
-			   <li class="app__menu-list-part">7.2 Расчет периметрального освещения</li>
-			   <li class="app__menu-list-part">7.3 Расчет спортивной площадки с учетом UGR-показателя</li>
-			   <li class="app__menu-list-part end">7.4 Расчет спортивной площадки с учетом GR-показателя</li>
+			   <li class="app__menu-list-part">5.2 Создание расчетных поверхностей</li>			   
+			   <li class="app__menu-list-part end">5.3 Расчет аварийного освещения</li>
+			   <li class="app__menu-list-head end">6 Расчет наружного освещения</li> 		   	   
+			   <li class="app__menu-list-head end">7 Создание отчета</li>
 			   <li class="app__menu-list-head end">8 Заключение</li>
 			</ul>
 
@@ -199,7 +194,7 @@ const AppMenu = ({onBurgerChange, onMenuOff}) => {
 						<p><a href="#">Рекламодателям</a></p>
 						<p><a href="#">Сотрудничество</a></p>
 					</div>
-					<p class="app__footer-copyright">© 2022 Copyright</p>
+					<p class="app__footer-copyright">© Р. Р. Исаев, 2023</p>
 				</footer>
 				
 	 </div>
